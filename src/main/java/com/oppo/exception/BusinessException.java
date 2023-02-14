f6acbec1c5ca4a4cc001edc5bd39f02f0e5d5570
lukang-local <1035525823@email.com>
2023-02-14 21:13:47 +0800

    add comoin

---
+++ src/main/java/com/oppo/exception/BusinessException.java
@@ -0,0 +1,35 @@
+package com.oppo.exception;
+
+public class BusinessException extends RuntimeException{
+    /**
+     * code
+     */
+    private Integer code;
+
+    /**
+     * message
+     */
+    private String message;
+
+    public BusinessException() {
+        super();
+    }
+
+    public BusinessException(Integer code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+        this.message = message;
+    }
+
+    public BusinessException(Throwable cause) {
+        this(null, cause.getMessage(), null);
+    }
+
+    public BusinessException(Integer code, String message) {
+        this(code, message, null);
+    }
+
+    public BusinessException(Integer code, Throwable cause) {
+        this(code, cause.getMessage(), cause);
+    }
+}
---
+++ src/main/java/com/oppo/exception/ExceptionCode.java
@@ -0,0 +1,10 @@
+package com.oppo.exception;
+
+public interface ExceptionCode {
+    public static final Integer PARAM_IS_NULL = 100000;
+
+    public static final Integer SYSTEM_ERROR = 100001;
+
+    public static final Integer NO_POWER = 100002;
+
+}
